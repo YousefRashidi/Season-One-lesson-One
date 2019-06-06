02817958a12837d676bef507e43b474aab69a882
YousefRashidi <yusef.r.r@gmail.com>
2019-06-06 21:40:12 +0430

    Create Season-One-lesson-One.py

---
+++ Season-One-lesson-One.py
@@ -0,0 +1,57 @@
+print("Hello")
+print("ُSalut")
+print("ُسلام")
+print("dos vidaniya")
+message = "hello"
+message2 = message
+print(message)
+print(message2)
+my_message = '"NAMING VARIABLES" \
+             "* a-z A-Z 0-9" \
+             "*lowecase only" \
+             "snake_case_convention for (camelCaseNaming, PascalCase)" \
+             "* _variable" \
+             "num_10" \
+             "X 5value"'
+print(my_message)
+متغیر = 10
+print(متغیر)
+num = 1
+value = 1.05
+A = 2+1j
+# یBy defining a new value for num
+num = 10
+num_2 = num + num
+print(num_2)
+# We can find the type of variable
+sample = type(num_2)
+sample2 = type(value)
+sample3 = type(my_message)
+sample4 = type (A)
+print(sample)
+print(sample2)
+print(sample3)
+print(sample4)
+# Convert variable types to each other
+sample_1 = float(num_2)
+sample_2 = int(value)
+print(num_2)
+print(sample_1)
+print(value)
+print(sample_2)
+new_int = int(value)
+print(new_int)
+tex = "10"
+sample_3 = int(tex)
+print(sample_3)
+# learning Python Id
+print("http://www.pythontutor.com/live.html#mode=edit")
+# list object
+people_name = "Yousef"
+people_lastname = "Rashidi"
+people_age = 28
+list_1 = ["Yousef" , "Rashidi", 28,]
+print(list_1)
+print(type(list_1))
+new_list =[people_name, people_lastname, people_age,]
+print(new_list)
